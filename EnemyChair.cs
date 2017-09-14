ec0f570069f382f551c1f7c9cc83610919f48ffc
DESKTOP-JNJDBSH\touho <takoyaki_4423@ezweb.ne.jp>
2017-09-14 15:29:57 +0900

    プログラム

---
+++ EnemyChair.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnemyChair : MonoBehaviour
+{
+    //化け物
+    public GameObject Bakemono;
+    //タイマー値保存変数
+    public float timer = 0;
+    //出すか出さないか
+    bool isAppear = false;
+    [SerializeField, Header("出現時間")]
+    private float appearTime = 10;
+
+
+    void Update()
+    {
+        if (timer > appearTime) return;
+
+        if (isAppear == true)
+        {
+            //タイマー開始
+            timer += Time.deltaTime;
+            //10秒以上カウントしたか
+            if (timer > appearTime)
+            {
+                //bakemono出す
+                Bakemono.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isAppear = true;
+            EventDoor.near = true;
+            //Debug.Log("kita");
+        }
+    }
+}
---
+++ EnemyHumanfigure.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class EnemyHumanfigure : MonoBehaviour
+{
+    public GameObject Bakemono;
+    public static bool isMove = false;
+    public Soundcontroller soucon;
+    public AudioSource audios;
+
+    private void Start()
+    {
+        audios.clip = soucon.audioclip01[2];
+        Bakemono.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (Bakemono != null && col.gameObject.tag == "Player")
+        {
+            Bakemono.SetActive(true);
+            audios.Play();
+            //Debug.Log("吾輩はママであるっ!!!!!");
+        }
+    }
+
+}
+
